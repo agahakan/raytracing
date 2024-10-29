a5dff083255ce3692f89dcf0f91fb5e35ea02717
agahakan <qirahakan@gmail.com>
2024-10-29 10:58:45 +0100

    feat: paramatized camera constructor

--- src/Camera/Camera.cpp
+++ src/Camera/Camera.cpp
@@ -1,9 +1,36 @@
 #include "Camera.hpp"
 
-void Camera::render(const Hittable &world, std::vector<Uint8> &pixels)
+Camera::Camera(double aspect_ratio, int image_width, int samples_per_pixel)
+    : aspect_ratio(aspect_ratio), image_width(image_width), samples_per_pixel(samples_per_pixel)
 {
     initialize();
+}
 
+void Camera::initialize()
+{
+    image_height = static_cast<int>(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    pixel_samples_scale = 1.0 / samples_per_pixel;
+
+    center = point3(0, 0, 0);
+
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (static_cast<double>(image_width) / image_height);
+
+    auto viewport_u = Vec3(viewport_width, 0, 0);
+    auto viewport_v = Vec3(0, -viewport_height, 0);
+
+    pixel_delta_u = viewport_u / image_width;
+    pixel_delta_v = viewport_v / image_height;
+
+    auto viewport_upper_left = center - Vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+    pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+}
+
+void Camera::render(const Hittable &world, std::vector<Uint8> &pixels)
+{
     pixels.resize(image_width * image_height * 3);
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
@@ -24,47 +51,24 @@ void Camera::render(const Hittable &world, std::vector<Uint8> &pixels)
     std::clog << "\rDone.                 \n";
 }
 
-void Camera::initialize()
-{
-    image_height = int(image_width / aspect_ratio);
-    image_height = (image_height < 1) ? 1 : image_height;
-
-    pixel_samples_scale = 1.0 / samples_per_pixel;
-
-    center = point3(0, 0, 0);
-
-    auto focal_length = 1.0;
-    auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * (double(image_width) / image_height);
-
-    auto viewport_u = Vec3(viewport_width, 0, 0);
-    auto viewport_v = Vec3(0, -viewport_height, 0);
-
-    pixel_delta_u = viewport_u / image_width;
-    pixel_delta_v = viewport_v / image_height;
-
-    auto viewport_upper_left = center - Vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
-    pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
-}
-
 Ray Camera::get_Ray(int i, int j) const
 {
-    // Construct a Camera Ray originating from the origin and directed at randomly sampled
-    // point around the pixel location i, j.
+    // Construct a camera ray originating from the origin and directed at a randomly sampled
+    // point around the pixel location (i, j).
 
     auto offset = sample_square();
     auto pixel_sample =
         pixel00_loc + ((i + offset.x()) * pixel_delta_u) + ((j + offset.y()) * pixel_delta_v);
 
-    auto Ray_origin = center;
-    auto Ray_direction = pixel_sample - Ray_origin;
+    auto ray_origin = center;
+    auto ray_direction = pixel_sample - ray_origin;
 
-    return Ray(Ray_origin, Ray_direction);
+    return Ray(ray_origin, ray_direction);
 }
 
 Vec3 Camera::sample_square() const
 {
-    // Returns the vector to a random point in the [-.5,-.5]-[+.5,+.5] unit square.
+    // Returns a vector to a random point in the [-0.5, -0.5] to [+0.5, +0.5] unit square.
     return Vec3(random_double() - 0.5, random_double() - 0.5, 0);
 }
 
@@ -78,6 +82,6 @@ color Camera::Ray_color(const Ray &r, const Hittable &world) const
     }
 
     Vec3 unit_direction = unit_vector(r.direction());
-    auto a = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
--- src/Camera/Camera.hpp
+++ src/Camera/Camera.hpp
@@ -13,15 +13,18 @@
 class Camera
 {
   public:
-    double aspect_ratio = 1.0;
-    int image_width = 100;
-    int image_height;
-    int samples_per_pixel = 10;
+    Camera(double aspect_ratio = 1.0, int image_width = 100, int samples_per_pixel = 10);
 
     void render(const Hittable &world, std::vector<Uint8> &pixels);
-    void initialize();
+
+    double aspect_ratio;
+    int image_width;
+    int image_height;
+    int samples_per_pixel;
 
   private:
+    void initialize();
+
     double pixel_samples_scale;
     point3 center;
     point3 pixel00_loc;
