b3a9a12121256af56f8441b35c836990db7e6150
Matteo <matteo.courquin@gmail.com>
2024-10-29 14:45:01 +0100

    feat: add assimp lib

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,6 +3,10 @@
 #include <memory>
 #include <thread>
 
+#include <assimp/Importer.hpp>
+#include <assimp/postprocess.h>
+#include <assimp/scene.h>
+
 #include "./Backend/SDLGraphics.hpp"
 #include "./Camera/Camera.hpp"
 #include "./Hittable/Hittable.hpp"
@@ -12,6 +16,16 @@
 #include "./Mesh/Triangle.hpp"
 #include "./Rtweekend/Rtweekend.hpp"
 
+void printVertices(const aiMesh *mesh)
+{
+    std::cout << "Vertices of mesh " << mesh->mName.C_Str() << ":\n";
+    for (unsigned int j = 0; j < mesh->mNumVertices; ++j) {
+        const aiVector3D &vertex = mesh->mVertices[j];
+        std::cout << "Vertex " << j << ": (" << vertex.x << ", " << vertex.y << ", " << vertex.z
+                  << ")\n";
+    }
+}
+
 int main()
 {
     HittableList world;
@@ -19,6 +33,23 @@ int main()
     Mesh sphere_mesh;
     generate_sphere_mesh(sphere_mesh, point3(0, 0, -1), 0.5, 4, 4);
     world.add(std::make_shared<Mesh>(sphere_mesh));
+
+    // PRINT MESH
+    Assimp::Importer importer;
+    const aiScene *scene =
+        importer.ReadFile("../assets/model.obj", aiProcess_Triangulate | aiProcess_FlipUVs);
+
+    if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) {
+        std::cerr << "Erreur lors de la chargement du modèle : " << importer.GetErrorString()
+                  << std::endl;
+        return -1;
+    }
+
+    for (unsigned int i = 0; i < scene->mNumMeshes; ++i) {
+        const aiMesh *mesh = scene->mMeshes[i];
+        printVertices(mesh);
+    }
+    // END PRINT MESH
 
     Camera cam(16.0 / 9.0, 400, 50);
 
