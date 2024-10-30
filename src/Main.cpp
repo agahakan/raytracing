75c5fe807bd171026befe6fe0c2b55169f63c2f3
Matteo <matteo.courquin@gmail.com>
2024-10-30 10:59:48 +0100

    feat: add comments and add plane into scene

--- src/Main.cpp
+++ src/Main.cpp
@@ -42,14 +42,30 @@ struct Sphere
     }
 };
 
+struct Plane
+{
+    Vec3 normal;
+    double distance;
+    Color color;
+
+    Plane(const Vec3 &n, double d, const Color &col)
+        : normal(n)
+        , distance(d)
+        , color(col)
+    {
+    }
+};
+
 class Scene
 {
   public:
     std::vector<Sphere> spheres;
+    std::vector<Plane> planes;
 
     Scene() {};
 
     void AddSphere(const Sphere &sphere) { spheres.emplace_back(sphere); }
+    void AddPlane(const Plane &plane) { planes.emplace_back(plane); }
 };
 
 struct Ray
@@ -64,11 +80,12 @@ struct Ray
     }
 };
 
+// INTERSECT RAY WITH SPHERE
 std::pair<double, double> IntersectRaySphere(const Ray &ray, const Sphere &sphere)
 {
     Vec3 CO = ray.origin - sphere.center;
-    double a = dot(ray.direction, ray.direction);
-    double b = 2 * dot(CO, ray.direction);
+    double a = dot(ray.direction, ray.direction);  // ? dot product
+    double b = 2 * dot(CO, ray.direction);  // ? dot product
     double c = dot(CO, CO) - sphere.radius * sphere.radius;
     double discriminant = b * b - 4 * a * c;
 
@@ -82,10 +99,23 @@ std::pair<double, double> IntersectRaySphere(const Ray &ray, const Sphere &spher
     return {t1, t2};
 }
 
+double IntersectRayPlane(const Ray &ray, const Plane &plane)
+{
+    double denominator = dot(ray.direction, plane.normal);
+    if (denominator == 0) {
+        return INF;
+    }
+
+    double t = -(dot(ray.origin, plane.normal) + plane.distance) / denominator;
+    return t;
+}
+
+// TRACE RAY AND RETURN COLOR
 Color TraceRay(const Ray &ray, double t_min, double t_max, const Scene &scene)
 {
     double closest_t = INF;
     const Sphere *closest_sphere = nullptr;
+    const Plane *closest_plane = nullptr;
 
     for (const auto &sphere : scene.spheres) {
         auto [t1, t2] = IntersectRaySphere(ray, sphere);
@@ -99,19 +129,34 @@ Color TraceRay(const Ray &ray, double t_min, double t_max, const Scene &scene)
         }
     }
 
-    if (!closest_sphere) {
-        return Color(0.2, 0.2, 0.2);  // Background color (grey)
+    for (const auto &plane : scene.planes) {
+        double t = IntersectRayPlane(ray, plane);
+        if (t >= t_min && t <= t_max && t < closest_t) {
+            closest_t = t;
+            closest_plane = &plane;
+        }
     }
-    return closest_sphere->color;
+
+    if (closest_plane) {
+        return closest_plane->color;
+    }
+
+    if (closest_sphere) {
+        return closest_sphere->color;
+    }
+
+    return Color(0.2, 0.2, 0.2);
 }
 
+// CONVERT SCREEN COORDINATES TO VIEWPORT COORDINATES
 Vec3 CanvasToViewport(int x, int y, int image_width, int image_height)
 {
-    double viewport_size = 1.0;
-    double projection_plane_d = 1.0;
+    double viewport_size = 1.0;  // ? viewport ration (1.0 = 100% = image size)
+    double projection_plane_d = 1.0;  // ? distance projection (z coordinate)
 
-    return Vec3(
-        x * viewport_size / image_width, y * viewport_size / image_height, projection_plane_d);
+    return Vec3(x * viewport_size / image_width,  // ? convert x to viewport coordinates
+                y * viewport_size / image_height,  // ? convert y to viewport coordinates
+                projection_plane_d);
 }
 
 int main()
@@ -121,21 +166,22 @@ int main()
     Image image(image_width, image_height);
     Scene scene;
 
-    Sphere redSphere(Vec3(0, -1, 3), 1, Color(1, 0, 0));
-    Sphere blueSphere(Vec3(2, 0, 4), 1, Color(0, 0, 1));
-    Sphere greenSphere(Vec3(-2, 0, 4), 1, Color(0, 1, 0));
+    Sphere redSphere(Vec3(0, -1, 3), 1, Color(1, 0, 0));  // ? red sphere
+    Sphere blueSphere(Vec3(2, 0, 4), 1, Color(0, 0, 1));  // ? blue sphere
+    Sphere greenSphere(Vec3(-2, 0, 4), 1, Color(0, 1, 0));  // ? green sphere
+    Plane plane(Vec3(0, 1, 0), 1, Color(1, 1, 1));  // ? white plane
 
     scene.AddSphere(redSphere);
     scene.AddSphere(blueSphere);
     scene.AddSphere(greenSphere);
+    scene.AddPlane(plane);
 
     const Vec3 O(0, 0, 0);
 
-    // Convert screen coordinates to be centered around 0
     for (int y = 0; y < image_height; ++y) {
         for (int x = 0; x < image_width; ++x) {
             double canvas_x = x - image_width / 2.0;
-            double canvas_y = -(y - image_height / 2.0);  // Flip y-axis
+            double canvas_y = -(y - image_height / 2.0);
 
             Vec3 D = CanvasToViewport(canvas_x, canvas_y, image_width, image_height);
             Ray ray(O, D);
